010737fd742d0f854efd2bf00a374e1c80224d4b
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-09 20:35:21 +0500

    working on the file path

--- template.py
+++ template.py
@@ -4,10 +4,29 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "cnnClassifier"
-
 
 
 list_of_files = [
-    ".github/workflows/.gitkeep"
+    ".github/workflows/.gitkeep",
+    "backend/ingest_data.py",
+    "backend/model.py",
+    "static/app.js",
+    "static/style.css",
+    "app.py",
+    ".env",
+    "Dockerfile",
+    "requirements.txt",
+    "templates/index.html",
 ]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
