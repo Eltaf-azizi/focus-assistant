0e8a91303a866fec2f7fcab21cbe1dda660df105
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-10 23:31:31 +0500

    working on the api key

--- backend/model.py
+++ backend/model.py
@@ -6,3 +6,21 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
 import gdown
+
+
+
+# Download .env file from gdrive
+file_id = ""
+url = f""
+output = ".env" # Change the filename as needed
+
+gdown.download(url, output, quiet=False)
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "db")
+
+
+# Ensure API key is set
+if not OPENAI_API_KEY:
+    raise ValueError("OpenAI API keu not found in .env")
