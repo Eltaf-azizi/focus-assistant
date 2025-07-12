784592ba02abe93cf953d0ab2f7ac78b6671095e
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-12 12:45:16 +0500

    worked on the functions

--- static/app.js
+++ static/app.js
@@ -6,5 +6,24 @@ async function askAssistant() {
 
 
     if (!query.trim()) {
+        responseDiv.innerHTML = "<p>Please enter a question.</p>";
+        return;
     }
+
+
+
+    responseDiv.innerHTML = "<p>Thinking...</p>";
+
+
+    let response = await fetch("/query", {
+        method: "POST",
+        headers: { "Content=Type": "application/json" },
+        body: JSON.stringify ({ query: query })
+    });
+
+
+    let result = await response.json();
+    responseDiv.innerHTML = `<p>${result.response}</p>`
+
 }
+
