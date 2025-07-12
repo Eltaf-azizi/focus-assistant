32f4954fcc61dd903b9b0251e1ad808f50a32325
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-12 12:21:28 +0500

    working on the javascript part

---
+++ static/app.js
@@ -0,0 +1,10 @@
+async function askAssistant() {
+
+    let query = document.getElementById("query").ariaValueMax;
+    let responseDiv = document.getElementById("response");
+
+
+
+    if (!query.trim()) {
+    }
+}
