a193b08d693b41767490b3e3bd78fd9098a3eaa1
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-15 00:13:50 +0500

    adding the app file

--- app.py
+++ app.py
@@ -1,45 +1,25 @@
-import os
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
-from backend.model import get_response
-
-
-
-# Load environment variables
-load_dotenv()
-
-
-os.system("python backend/ingest_data.py")
-
+from src.prompt import *
+import os
 
 app = Flask(__name__)
-CORS(app)
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+embeddings = download_hugging_face_embeddings()
 
 
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-
-@app.route("/query", methods=["POST"])
-def query_assistant():
-    data = request.json
-    user_query = data.get("query", "")
-
-
-    if not user_query:
-        return jsonify({"error": "Query cannot be empty"}), 400
-    
-
-    response = get_response(user_query)
-    return jsonify({"response": response})
-
-
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+index_name = "medicalbot"
